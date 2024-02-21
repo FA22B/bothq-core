21f31db57fa37208c44226e269c5e1bf3d1ed708
Spencernator <150229110+Spencernator@users.noreply.github.com>
2024-02-21 12:20:29 +0100

    Created Spring Boot Project Java 21

---
+++ src/main/java/com/bothq/core/bothqcore/BothqCoreApplication.java
@@ -0,0 +1,13 @@
+package com.bothq.core.bothqcore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BothqCoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BothqCoreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bothq/core/bothqcore/BothqCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bothq.core.bothqcore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BothqCoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
