2fb3a7dbfebb20e3b6262155a0b97542508ed6d4
Spencernator <150229110+Spencernator@users.noreply.github.com>
2024-03-01 14:45:10 +0100

    Added JDA auto configuration

---
+++ src/main/java/com/bothq/core/discord/configuration/JdaConfiguration.java
@@ -0,0 +1,12 @@
+package com.bothq.core.discord.configuration;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@Getter
+@Setter
+@ConfigurationProperties("spring.discord")
+public class JdaConfiguration {
+    private String token;
+}
