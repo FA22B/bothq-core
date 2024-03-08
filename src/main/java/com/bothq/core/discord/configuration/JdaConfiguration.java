3129b51e727f25e4db2a38d7aaa4f892c31771ab
Spencernator <150229110+Spencernator@users.noreply.github.com>
2024-03-08 16:10:33 +0100

    Added proxy support for JDA connection

--- src/main/java/com/bothq/core/discord/configuration/JdaConfiguration.java
+++ src/main/java/com/bothq/core/discord/configuration/JdaConfiguration.java
@@ -9,4 +9,6 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 @ConfigurationProperties("spring.discord")
 public class JdaConfiguration {
     private String token;
+    private String proxyHost;
+    private int proxyPort;
 }
